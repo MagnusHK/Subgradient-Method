ef1520a756ee87464b5e9fe9e9df5b2fee76b286
MagnusHK <74245680+MagnusHK@users.noreply.github.com>
2023-10-24 10:56:45 +0200

    Added implementation of the simple subgradient method, SubgradientMethod.jl

---
+++ SubgradientMethod.jl
@@ -0,0 +1,36 @@
+# Implementation of a simple subgradient method for solving the Lagrangian Dual
+
+# Knapsack problem
+# Z = max cx
+# s.t. Dx <= d
+# x in {0,1}^3
+
+c = [11, 5, 14]
+D = [3, 2, 4]
+d = 5
+
+# Initialize the Lagrange multipliers
+u = 5
+
+# Iterations
+T = 100
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
