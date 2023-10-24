c1c5842ac26b78663dddfc871cd1e51651f15ba9
MagnusHK <74245680+MagnusHK@users.noreply.github.com>
2023-10-24 11:26:15 +0200

    Updates to the subgradient procedure, not done yet

--- SubgradientMethod.jl
+++ SubgradientMethod.jl
@@ -1,22 +1,54 @@
 # Implementation of a simple subgradient method for solving the Lagrangian Dual
 
+
+# Import packages
+using JuMP, GLPK, LinearAlgebra
+
+
 # Knapsack problem
 # Z = max cx
 # s.t. Dx <= d
 # x in {0,1}^3
 
-c = [11, 5, 14]
-D = [3, 2, 4]
-d = 5
-
-# Initialize the Lagrange multipliers
-u = 5
+c = [11.0, 5.0, 14.0]
+D = [3.0 2.0 4.0]
+d = 5.0
 
 # Iterations
-T = 100
+n = 100
 
+# Initialize the Lagrange multipliers
+u = ones(1,n)
 
+# Define 0 < pi < 2
+pi = 1
 
+# Define model
+model = Model(GLPK.Optimizer)
+
+@variable(model, x[1:3], Bin)
+
+for t in 1:n
+
+    @objective(model, Min, sum(c[i]*x[i] for i=1:3) + u[t]*(d-sum(D[i]*x[i] for i=1:3)))
+
+    # Solve Lagrangian subproblem IP(u(t))
+    JuMP.optimize!(model)
+
+    # Solution
+    xsol = JuMP.value.(x)
+    z_u = JuMP.objective_value(model)
+
+    # Compute the current violation of the 'complicated' constraints
+    s = d-(D*xsol)[1]
+
+    # Compute T
+    T = pi*(z_ub - z_u)/s
+
+    # Iterate u
+    u[t+1] = max(0,u[t]+T*s)
+    
+end
 
 
 
